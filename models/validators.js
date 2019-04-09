785a52f9a7fe60a62cd17f021d16b4e7b49ef55a
Tim Vlasuk <timvlasuk@gmail.com>
2019-04-09 14:19:20 -0700

    grouped files for export in index.js; made seperate file to hold custom validators

---
+++ models/validators.js
@@ -0,0 +1,17 @@
+
+
+
+function email(email) {
+
+  if(typeof email != "string") {
+    return false
+  }
+  if(!email.includes('@')) {
+    console.log('faltz')
+  }
+}
+
+const VALID_STYLES = ['Tap', 'Ballet', 'Swing', 'Salsa', 'Flaminca']
+
+
+module.exports = { email, VALID_STYLES }
