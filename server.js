8a29c438d44d8351052686ee96d68d8e5010469a
Tim Vlasuk <timvlasuk@gmail.com>
2019-04-09 14:19:56 -0700

    setup mongoose server and test endpoint for viewing data

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
 
-const { PORT, NODE_ENV } = require('./config');
+const { PORT, NODE_ENV, MONGODB_URI } = require('./config');
+const  Course  = require('./models/course')
 
 let banner = `Welcome`;
+
+app.use(cors());
 
 app.use(morgan(
   NODE_ENV === 'development' ? 'dev' : 'tiny',
@@ -16,7 +19,6 @@ app.use(morgan(
   }
 ));
 
-app.use(cors());
 
 
 app.get('/', (req, res) => {
@@ -25,8 +27,53 @@ app.get('/', (req, res) => {
   res.json(banner).status(200)
 });
 
-app.listen(PORT, function () {
-  if (NODE_ENV === 'development') {
-    console.log(`Listening on port ${this.address().port}`)
-  }
-});
+
+
+
+app.get('/too', (req, res) => {
+  console.log('trighnahGetzum')
+
+  Course.findOne()
+  .then(style => {
+    console.log(style)
+    res.status(201).json(style)
+  })
+
+  
+})
+
+
+
+
+
+
+let server;
+
+function runServer() {
+  mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, err => {
+    if(err) {
+      console.log(err)
+      reject(err);
+    }
+
+    server = app.listen(PORT, function () {
+      
+      if (NODE_ENV === 'development') {
+        console.log(`Glistening on port ${this.address().port}`)
+      }
+
+      console.log(`connected to ${MONGODB_URI}`)
+      })
+
+  });
+
+};
+
+
+
+
+
+if (require.main === module) {
+  runServer()
+}
+
