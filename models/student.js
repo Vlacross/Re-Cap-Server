0d1798da7c1d34985b6bba28275d91b829722796
Tim Vlasuk <timvlasuk@gmail.com>
2019-04-09 23:29:05 -0700

    switched to using mongoose discrimination for different types of user data (student, teacher)

---
+++ models/student.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const User = require('./user');
+
+const { email } = require('./validators');
+var custom = [email, 'Must have Valid Email format'];
+
+
+const studentSchema = new Schema({
+  enrolled: { type: Boolean, required: true },
+  courses: [{
+    type: ObjectId,
+    ref: 'Course',
+    required: function () { return this.status || this.isTeacher }
+  }]
+});
+
+const Student = User.discriminator('Student', studentSchema);
+
+
+
+
+
+
+module.exports = mongoose.model('Student', studentSchema)
