be91bdce35c9c5af08c07a4fef9660a2880f2560
Tim Vlasuk <timvlasuk@gmail.com>
2019-04-18 23:30:12 -0700

    configured proper algorithm

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/dance-class',
   TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/dance-class-test',
-  ALGORITHM: process.env.ALGORITHM || 'sha256',
+  ALG: process.env.ALGORITHM || 'HS256',
   JWT_SECRET: process.env.JWT_SECRET || 'milldrew',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
   NODE_ENV: process.env.NODE_ENV || 'development'
