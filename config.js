8418fce4b9bdeead7b8650418b9772731cc82f92
Tim Vlasuk <timvlasuk@gmail.com>
2019-04-24 15:41:29 -0700

    moving towards deploy

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: 'mongodb://localhost/dance-class' || process.env.MONGODB_URI,
-  TEST_MONGODB_URI: 'mongodb://localhost/dance-class' || process.env.TEST_MONGODB_URI,
+  MONGODB_URI: process.env.MONGODB_URI,
+  TEST_MONGODB_URI:  process.env.TEST_MONGODB_URI,
   ALG: process.env.ALGORITHM,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
